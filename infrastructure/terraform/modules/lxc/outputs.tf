dd9dc4002f60674613aa2f84614b03fa0f4e87ac
jason.mcmullen@gmail.com <jason.mcmullen@gmail.com>
2025-12-30 22:53:53 -0400

    revert

--- infrastructure/terraform/modules/lxc/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "name" {
-  description = "Resource name requested"
-  value       = var.name
-}
-
-output "example_message" {
-  value = "This module is a scaffold — replace with provider resource outputs like vmid and ip"
-}
